36b33934b8b525e97952504fd71fc8c5ab3b0c2b
slovenoya <rzhang21@calpoly.edu>
2021-10-03 14:43:42 -0700

    try on 4-3

--- sample_backend.py
+++ sample_backend.py
@@ -76,7 +76,7 @@ def get_users():
       user_to_add = request.get_json()
       user_to_add['id'] = generate_id()
       users['users_list'].append(user_to_add)
-      resp = jsonify(success=True)
+      resp = jsonify(success=True, new_id=user_to_add['id'])
       resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
